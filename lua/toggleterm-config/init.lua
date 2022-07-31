937a56bb5480832503e88a9806acf9705dfcbccc
kokweikhong <kokweikhong@gmail.com>
2022-07-31 23:42:49 +0800

    update toggleterm setting

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap("n", "<leader>tt", ":ToggleTerm direction=float<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>tb", ":ToggleTerm size=10<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>tr", ":ToggleTermToggleAll<CR>", {noremap = true, silent = true})
 
